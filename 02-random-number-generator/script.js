fa3f045abb18159beb9c1125fc5a981ac9d770ec
HuyHoangNguyen32 <92217902+HuyHoangNguyen32@users.noreply.github.com>
2023-06-02 10:57:40 +0900

    🚀 02 random number generator

---
+++ 02-random-number-generator/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/* ##################################################
+                  ? : Solution 1
+################################################## */
+// function randomNumber() {
+//   return Math.trunc(Math.random() * 10 + 1);
+// }
+// const showNumber = document.querySelector('.number');
+// const randomButton = document.querySelector('.random-button');
+
+// if (randomButton) {
+//   randomButton.addEventListener('click', () => {
+//     const number = randomNumber();
+//     showNumber.textContent = number;
+//   });
+// }
+
+/* ##################################################
+                  ? : Solution 2
+################################################## */
+const showNumber = document.querySelector('.number');
+const randomButton = document.querySelector('.random-button');
+
+const generateNumber = () => {
+  const randomNumber = Math.floor(Math.random() * 10 + 1);
+  showNumber.innerHTML = randomNumber;
+};
+
+randomButton.addEventListener('click', generateNumber);
